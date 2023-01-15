df8a6314c5d613181a97e6a98b37b8001b177cb7
mikmum <cobblechups@gmail.com>
2023-01-15 21:12:02 +0100

    Rename tests to describe purpose

--- src/test/java/com/epam/bootcamp/bmi_calculator/BMITest.java
+++ src/test/java/com/epam/bootcamp/bmi_calculator/BMITest.java
@@ -3,24 +3,10 @@ package com.epam.bootcamp.bmi_calculator;
 import static org.junit.Assert.*;
 
 import org.junit.*;
-import org.junit.rules.ExpectedException;
-
-import com.epam.bootcamp.bmi_calculator.interfacesImplements.MetricBMI;
 
 public class BMITest {
 	
 	App app;
-	
-	/*@Test
-	public void GuessTheUnitsTest1() throws Exception{
-		GuessTheUnits gtu = new GuessTheUnits(1.7,50.0);
-		assertEquals(gtu.getUnitType(),"metric");
-	}
-	
-	@Test
-	public void GuessTheUnitsTest2() throws Exception{
-		
-	}*/
 	
 	@Before
 	public void setup(){
@@ -28,7 +14,7 @@ public class BMITest {
 	}
 	
 	@Test
-	public void BMITest1() throws Exception{
+	public void shouldReturnThinnessForHeightInMetersAndWeightInKgs() throws Exception{
 		app.setHeight(1.7);
 		app.setWeight(50);
 		assertEquals(app.calculateBMI(),17.3,1);
@@ -36,7 +22,7 @@ public class BMITest {
 	}
 	
 	@Test
-	public void BMITest2() throws Exception{
+	public void shouldThrowExceptionForHeightZero() throws Exception{
 		try{
 			app.setHeight(0);
 			app.setWeight(50.0);
@@ -47,7 +33,7 @@ public class BMITest {
 	}
 	
 	@Test
-	public void BMITest3() throws Exception{
+	public void shouldThrowExceptionForNegativeWeight() throws Exception{
 		try{
 			app.setHeight(1);
 			app.setWeight(-3);
@@ -58,7 +44,7 @@ public class BMITest {
 	}
 	
 	@Test
-	public void BMITest4() throws Exception{
+	public void shouldThrowExceptionForDifferentMetricUsed() throws Exception{
 		try{
 			app.setHeight(170);
 			app.setWeight(1764);
@@ -69,7 +55,7 @@ public class BMITest {
 	}
 	
 	@Test
-	public void BMITest5() throws Exception{
+	public void shouldReturnThinnessForHeightInCmAndWeightInKgs() throws Exception{
 		app.setHeight(170);
 		app.setWeight(50.0);
 		assertEquals(app.calculateBMI(),17.3,1);
@@ -77,7 +63,7 @@ public class BMITest {
 	}
 	
 	@Test
-	public void BMITest6() throws Exception{
+	public void shouldReturnThinnessForHeightInInchesAndWeightInOunces() throws Exception{
 		app.setHeight(5.58);
 		app.setWeight(1764);
 		assertEquals(app.calculateBMI(),17.3,1);
@@ -85,7 +71,7 @@ public class BMITest {
 	}
 	
 	@Test
-	public void BMITest7() throws Exception{
+	public void shouldReturnCorrectBMIForHeightInInchesAndWeightInPounds() throws Exception{
 		app.setHeight(86.7);
 		app.setWeight(6349.3);
 		assertEquals(app.calculateBMI(),37.19,0.5);
@@ -93,7 +79,7 @@ public class BMITest {
 	}
 	
 	@Test
-	public void BMITest8() throws Exception{
+	public void shouldReturnCorrectBMIForHeightInFeetAndWeightInKgs() throws Exception{
 		app.setHeight(6.56);
 		app.setWeight(220.47);
 		assertEquals(app.calculateBMI(),25,0.5);
@@ -101,7 +87,7 @@ public class BMITest {
 	}
 	
 	@Test
-	public void BMITest9() throws Exception{
+	public void shouldReturnCorrectBMIForHeightInCmAndWeightInKgs() throws Exception{
 		app.setHeight(200);
 		app.setWeight(80);
 		assertEquals(app.calculateBMI(),20,0.0);
--- src/test/java/com/epam/bootcamp/bmi_calculator/GuessTheUnitsTest.java
+++ src/test/java/com/epam/bootcamp/bmi_calculator/GuessTheUnitsTest.java
@@ -5,34 +5,34 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class GuessTheUnitsTest {
-	
-	@Test
-	public void GuessTheUnitsTest1() throws Exception{
-		GuessTheUnits gtu = new GuessTheUnits(1.7,50.0);
-		assertEquals(gtu.getUnitType(),"metric");
-	}
-	
-	@Test
-	public void GuessTheUnitsTest2() throws Exception{
-		GuessTheUnits gtu = new GuessTheUnits(170,50.0);
-		assertEquals(gtu.getUnitType(),"metric");
-	}
-	
-	@Test
-	public void GuessTheUnitsTest3() throws Exception{
-		GuessTheUnits gtu = new GuessTheUnits(5.58,1764);
-		assertEquals(gtu.getUnitType(),"US");
-		assertEquals(gtu.getWeight(),110,1);
-	}
-	
-	@Test
-	public void GuessTheUnitsTest4() throws Exception{
-		try{
-			GuessTheUnits gtu = new GuessTheUnits(170,1764);
-			assertEquals(gtu.getUnitType(),"US");
-		}catch(Exception e){
-			assertEquals(e.getMessage(),"Height and weight is in different metric.");
-		}
-	}
-	
+
+    @Test
+    public void shouldRecognizeMetersAndKilosAsMetric() throws Exception {
+        GuessTheUnits gtu = new GuessTheUnits(1.7, 50.0);
+        assertEquals(gtu.getUnitType(), "metric");
+    }
+
+    @Test
+    public void shouldRecognizeCmsAndKilosAsMetric() throws Exception {
+        GuessTheUnits gtu = new GuessTheUnits(170, 50.0);
+        assertEquals(gtu.getUnitType(), "metric");
+    }
+
+    @Test
+    public void shouldRecognizeFeetAndOuncesAsUS() throws Exception {
+        GuessTheUnits gtu = new GuessTheUnits(5.58, 1764);
+        assertEquals(gtu.getUnitType(), "US");
+        assertEquals(gtu.getWeight(), 110, 1);
+    }
+
+    @Test
+    public void shouldThrowExceptionOnDifferentMetrics() {
+        try {
+            GuessTheUnits gtu = new GuessTheUnits(170, 1764);
+            assertEquals(gtu.getUnitType(), "US");
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "Height and weight is in different metric.");
+        }
+    }
+
 }
